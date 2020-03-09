f9e7744e0cff922d056999bfdbc3e6477af86c6f
Lucas Wilber <lucasjwilber@gmail.com>
2020-03-08 20:49:07 -0700

    added sensor monitoring to Main

--- app/src/main/java/com/vik/barbershop/AudioRecorder.java
+++ app/src/main/java/com/vik/barbershop/AudioRecorder.java
@@ -1,5 +1,9 @@
 package com.vik.barbershop;
 
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
@@ -7,7 +11,15 @@ import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
+import android.media.audiofx.PresetReverb;
+
 public class AudioRecorder extends Thread {
+
+
+
 
     private boolean stopped = false;
 
@@ -16,8 +28,10 @@ public class AudioRecorder extends Thread {
      */
     public AudioRecorder() {
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+
         start();
     }
+
 
     @Override
     public void run() {
@@ -35,18 +49,42 @@ public class AudioRecorder extends Thread {
             int N = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
             recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, N * 10);
             track = new AudioTrack(AudioManager.STREAM_MUSIC, 8000,
-                    AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, N * 10, AudioTrack.MODE_STREAM);
+                    AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, N * 5, AudioTrack.MODE_STREAM);
+
             recorder.startRecording();
+
             track.play();
+
+            //doesn't do anything
+//            PresetReverb reverb = new PresetReverb(0, track.getAudioSessionId());
+//            reverb.setPreset(PresetReverb.PRESET_LARGEHALL);
+
+
+
             /*
              * Loops until something outside of this thread stops it.
              * Reads the data from the recorder and writes it to the audio track for playback.
              */
+            long count = 0;
             while (!stopped) {
-                Log.i("Map", "Writing new data to buffer");
+//                Log.i("Map", "Writing new data to buffer");
+
                 short[] buffer = buffers[ix++ % buffers.length];
+
+
                 N = recorder.read(buffer, 0, buffer.length);
+
+
+                for (int i = 0; i < buffer.length; i++) {
+//                    buffer[i] *= 20;
+                }
+
                 track.write(buffer, 0, buffer.length);
+
+                count++;
+                if (count % 200 == 0) {
+                    Log.i("ljw", Arrays.toString(buffer));
+                }
             }
         } catch (Throwable x) {
             Log.w("Audio", "Error reading voice audio", x);
@@ -67,4 +105,5 @@ public class AudioRecorder extends Thread {
     private void close() {
         stopped = true;
     }
+
 }
--- app/src/main/java/com/vik/barbershop/MainActivity.java
+++ app/src/main/java/com/vik/barbershop/MainActivity.java
@@ -3,17 +3,29 @@ package com.vik.barbershop;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     AudioRecorder audioRecorder;
     int REQUEST_MICROPHONE = 69;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private float lastX, lastY, lastZ;
+    private float deltaXMax = 0;
+    private float deltaYMax = 0;
+    private float deltaZMax = 0;
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +34,54 @@ public class MainActivity extends AppCompatActivity {
 
         //check for RECORD_AUDIO permission, ask for it if we don't have it, then start recording
         requestRecordAudioPermission();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            Log.i("ljw", "success! we have an accelerometer");
+
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            Log.i("ljw", "fail! we dont have an accelerometer!");
+        }
     }
+
+
+    //onResume() register the accelerometer for listening the events
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // get the change of the x,y,z values of the accelerometer
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        // if the change is below 2, it is just plain noise
+
+        if (deltaX < 2) deltaX = 0;
+        if (deltaY < 2) deltaY = 0;
+        if (deltaZ < 2) deltaZ = 0;
+
+        Log.i("ljw", "x: " + deltaX);
+        Log.i("ljw", "y: " + deltaY);
+        Log.i("ljw", "z: " + deltaZ);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //necessary for interface implementation but not being used currently
+    }
+
+    //onPause() unregister the accelerometer for stop listening the events
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
 
     public void requestRecordAudioPermission() {
         if (ContextCompat.checkSelfPermission(this,
@@ -36,8 +95,6 @@ public class MainActivity extends AppCompatActivity {
             audioRecorder = new AudioRecorder();
         }
     }
-
-
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
 
@@ -59,6 +116,12 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("audio", "RECORD_AUDIO permission denied");
             }
         }
+
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
